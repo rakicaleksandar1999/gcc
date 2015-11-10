b83a7a0b4510aeab71874818b87e23c12a475b28
Matthew Fortune <matthew.fortune@imgtec.com>
2015-11-10 15:34:20 +0000

    Add support for a limit for inlining memcpy
    Expose it with an option: -mblockmov-limit. A memcpy strictly less than
    this value will be considered for inlining.
    
    gcc/ChangeLog:
    
    	* config/mips/mips.cc (mips_expand_block_move): Add support to
    	control size of inlined memcpy.
    	* config/mips/mips.opt (mblockmov-limit): New option.
    
    Cherry-picked cf1e4960a4f80301e4c8f71a35cbbc8fef1ce6fd
    from https://github.com/MIPS/gcc
    
    Signed-off-by: Matthew Fortune <matthew.fortune@imgtec.com>
    Signed-off-by: Faraz Shahbazker <fshahbazker@wavecomp.com>
    Signed-off-by: Aleksandar Rakic <aleksandar.rakic@htecgroup.com>

--- gcc/config/mips/mips.cc
+++ gcc/config/mips/mips.cc
@@ -9360,16 +9360,19 @@ mips_expand_block_move (rtx dest, rtx src, rtx length)
 	  || MEM_ALIGN (dest) < MIPS_MIN_MOVE_MEM_ALIGN))
     return false;
 
-  if (INTVAL (length) <= MIPS_MAX_MOVE_BYTES_PER_LOOP_ITER)
+  if (mips_movmem_limit == -1 || INTVAL (length) < mips_movmem_limit)
     {
-      mips_block_move_straight (dest, src, INTVAL (length));
-      return true;
-    }
-  else if (optimize)
-    {
-      mips_block_move_loop (dest, src, INTVAL (length),
-			    MIPS_MAX_MOVE_BYTES_PER_LOOP_ITER);
-      return true;
+      if (INTVAL (length) <= MIPS_MAX_MOVE_BYTES_PER_LOOP_ITER)
+  {
+    mips_block_move_straight (dest, src, INTVAL (length));
+    return true;
+  }
+      else if (optimize)
+  {
+    mips_block_move_loop (dest, src, INTVAL (length),
+			  MIPS_MAX_MOVE_BYTES_PER_LOOP_ITER);
+    return true;
+  }
     }
 
   return false;
